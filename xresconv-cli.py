290dcbbd1fc31f891bb0ad8998387634a02db591
WenTao Ou <admin@owent.net>
2022-03-14 19:58:40 +0800

    Fix missing path

--- xresconv-cli.py
+++ xresconv-cli.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import os
+
 if __name__ == "__main__":
+    script_dir = os.path.dirname(__file__)
+    sys.path.append(script_dir)
     from xresconv_cli import main
 
     main()
